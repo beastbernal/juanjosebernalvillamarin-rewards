612debcc2f2e8d225085800b4ef182602f558289
jj bernal <beastbernal@gmail.com>
2020-08-31 11:40:51 -0500

    Updates

---
+++ precache-manifest.83b22ec35ae9e6f4f361fa9d981a663b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e550075b696595c1156d0907db19babb",
+    "url": "/juanjosebernalvillamarin-rewards/index.html"
+  },
+  {
+    "revision": "59262300d6fbe5000f5b",
+    "url": "/juanjosebernalvillamarin-rewards/static/css/main.0de45c0c.chunk.css"
+  },
+  {
+    "revision": "79ea7ab79287e1594b33",
+    "url": "/juanjosebernalvillamarin-rewards/static/js/2.55633fd8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/juanjosebernalvillamarin-rewards/static/js/2.55633fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59262300d6fbe5000f5b",
+    "url": "/juanjosebernalvillamarin-rewards/static/js/main.0ccd8d13.chunk.js"
+  },
+  {
+    "revision": "8ea4a882846ebe278bd9",
+    "url": "/juanjosebernalvillamarin-rewards/static/js/runtime-main.1be35997.js"
+  },
+  {
+    "revision": "cf518ba3984a889130b252db4a584f00",
+    "url": "/juanjosebernalvillamarin-rewards/static/media/logo.cf518ba3.png"
+  }
+]);
