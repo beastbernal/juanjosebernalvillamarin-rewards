e630a27db83a022ce0ef61afd59b1850b9fba4b7
jj bernal <beastbernal@gmail.com>
2020-09-01 00:20:02 -0500

    Se corrigen las imagenes de las cards, se agrega la funcionalidad de consulta con el token, se ajusta la consulta de la persona, se ajusta la consulta de los objetos, se implementa el useMemo para pasar varios objetos, lo probe con useState para un objeto pero me parece que funciona mas rapido con useMemo

--- src/components/DinosaurStore.js
+++ src/components/DinosaurStore.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { AppContext } from "../context/ContextProvider";
 import { makeStyles } from "@material-ui/core/styles";
 // import { MenuIcon, MoreIcon, SearchIcon } from "@material-ui/icons/Menu";
-import Logo from '../assets/logo.png'
+import Logo from "../assets/logo.png";
 import {
   Card,
   CardActionArea,
@@ -21,10 +21,9 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   rootbar: {
-    position: 'fixed',
-    width: '100%',
+    position: "fixed",
+    width: "100%",
     zIndex: 1000,
-    
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -34,13 +33,13 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "flex-start",
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(2),
-    background: 'linear-gradient(45deg, #8fb243 30%, #d36c1c 90%)',
+    background: "linear-gradient(45deg, #8fb243 30%, #d36c1c 90%)",
     border: 0,
     borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
     height: 48,
-    padding: '0 10px',
+    padding: "0 10px",
     zIndex: 1000,
   },
   title: {
@@ -49,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
   },
   container: {
     paddingTop: theme.spacing(20),
-  }
+  },
 }));
 
 const DinosaurStore = () => {
@@ -74,16 +73,21 @@ const DinosaurStore = () => {
       <div className={classes.rootbar}>
         <AppBar position="static">
           <Toolbar className={classes.toolbar}>
-            <img src={Logo} alt="logo"/>
+            <img src={Logo} alt="logo" />
             <Typography className={classes.title} variant="h5" noWrap>
               Dinosaur Store
             </Typography>
+            <Typography
+              className={classes.title}
+              variant="h5"
+              noWrap
+            ><UserInfo/> hola</Typography>
           </Toolbar>
         </AppBar>
       </div>
       <div className={classes.container}>
         <Grid container spacing={1}>
-          <Grid container item xs={12} spacing={3}>
+          <Grid container item xs={12} spacing={4}>
             <ProductList title="Libros por leer" />
           </Grid>
         </Grid>
@@ -92,23 +96,28 @@ const DinosaurStore = () => {
   );
 };
 
+function UserInfo({}) {
+  const { providerValue: {userData = []} } = useContext(AppContext);
+  return (
+      <div> {userData.name}</div>
+  )
+
+}
+
 function ProductList({ title }) {
-  const { products } = useContext(AppContext);
-  
+  const { providerValue: {products = []} } = useContext(AppContext);
   const classes = useStyles();
   return (
-    // <div>
     <>
-      {products.map((product) => {
+      {products && products.map((product) => {
         return (
           <React.Fragment key={product._id}>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <Card className={classes.root}>
                 <CardActionArea>
                   <CardMedia
                     key={product._id}
                     component="img"
-                    height="140"
                     image={product.img.url}
                     title={product.name}
                   />
@@ -120,7 +129,7 @@ function ProductList({ title }) {
                       variant="body2"
                       color="textSecondary"
                       component="p"
-                      >
+                    >
                       {product.category}
                     </Typography>
                   </CardContent>
@@ -141,46 +150,5 @@ function ProductList({ title }) {
     </>
   );
 }
-
-// function ListaLibros({ title, readCondition }) {
-//   const { libros, setLibros } = useContext(AppContext);
-// const handleChange = event => {
-//   event.persist();
-//   const { id, checked } = event.target;
-//   const modifiedLibros = libros.map(libro => {
-//     if (libro.id === id) {
-//       return {
-//         ...libro,
-//         leido: checked
-//       };
-//     }
-//     return libro;
-//   });
-//   setLibros(modifiedLibros);
-// };
-//   return (
-//     <div>
-//       <div className="context-title">{title}</div>
-//       <div className="flex column libros">
-//         {/* {libros
-//           .filter(libro => libro.leido === readCondition)
-//           .map(libro => {
-//             return (
-//               <div key={libro.id} className="libro">
-//                 <span>{libro.nombre}</span>
-//                 <input
-//                   id={libro.id}
-//                   onChange={handleChange}
-//                   checked={libro.leido}
-//                   className="check"
-//                   type="checkbox"
-//                 />
-//               </div>
-//             );
-//           })} */}
-//       </div>
-//     </div>
-//   );
-// }
 
 export default DinosaurStore;
